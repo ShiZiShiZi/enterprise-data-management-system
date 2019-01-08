4451526007884c710b260164cbbf46ef550f1995
user <392457608@qq.com>
2019-01-08 23:07:39 +0800

    Adding people to the project coding completion. Ref #106

---
+++ backend/src/main/java/org/hfut/service/global/ProjectPeopleSearchService.java
@@ -0,0 +1,60 @@
+package org.hfut.service.global;
+
+import org.hfut.mapper.ProjectPeopleMapper;
+import org.hfut.pojo.ProjectPeople;
+import org.hfut.pojo.ProjectPeopleExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Lee
+ * @date 2019/1/7
+ **/
+@Service
+public class ProjectPeopleSearchService {
+    @Autowired
+    private ProjectPeopleMapper projectPeopleMapper;
+
+    public List<ProjectPeople> listProjectPeople(List<Integer> projectId, String name, Integer isAcrossDepartment,
+                                                 Integer departmentId, String sortColumn, Integer sortOrder) {
+        List<ProjectPeople> list = new ArrayList<>();
+        String str = "0";
+        Integer num = -1;
+        Integer num1 = 0;
+        Integer num2 = 1;
+        ProjectPeopleExample projectPeopleExample = new ProjectPeopleExample();
+        ProjectPeopleExample.Criteria criteria = projectPeopleExample.createCriteria();
+        if (!projectId.isEmpty()) {
+            for (int i = 0; i < projectId.size(); i++) {
+                criteria.andIdEqualTo(projectId.get(i));
+            }
+        }
+        if (!name.equals(str)) {
+            criteria.andNameLike("%" + name + "%");
+        }
+        if (isAcrossDepartment.equals(num1)) {
+            criteria.andAcrossDepartmentEqualTo(false);
+        }
+        if (isAcrossDepartment.equals(num2)) {
+            criteria.andAcrossDepartmentEqualTo(true);
+        }
+        if (!departmentId.equals(num)) {
+            criteria.andDepartmentIdEqualTo(departmentId);
+        }
+        if (sortOrder.equals(num)) {
+            sortColumn += " DESC";
+        }
+        if (sortOrder.equals(num2)) {
+            sortColumn += " ASC";
+        }
+        projectPeopleExample.setOrderByClause(sortColumn);
+        list = projectPeopleMapper.selectByExample(projectPeopleExample);
+        for (int j = 0; j < list.size(); j++) {
+            list.get(j).setPassword(null);
+        }
+        return list;
+    }
+}
