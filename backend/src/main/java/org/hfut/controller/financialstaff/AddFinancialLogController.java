e06f3764b413fb46d21e97a1d7258e08904967c3
卅果果 <1783966147@qq.com>
2019-01-12 13:47:22 +0800

    Modified the location of the package. Ref #155

--- backend/src/main/java/org/hfut/controller/financialstaff/AddFinancialLogController.java
+++ backend/src/main/java/org/hfut/controller/financialstaff/AddFinancialLogController.java
@@ -1,7 +1,7 @@
 package org.hfut.controller.financialstaff;
 
 import jdk.nashorn.internal.parser.Token;
-import org.hfut.service.AddFinancialLogService;
+import org.hfut.service.financialstaff.AddFinancialLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
